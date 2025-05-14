1fd9f40fac2e587bf17431c7fd66d7e1914cf3d9
Arvac972 <andyst.boston@gmail.com>
2025-05-14 10:43:32 -0400

    Update chessUI.py

--- chessUI.py
+++ chessUI.py
@@ -255,15 +255,3 @@ if __name__ == "__main__":
     root = tk.Tk()
     app = ChessUI(root)
     root.mainloop()
-
-
-
-pieces = {  
-            
-            'pion' : ("sus"),
-            'fou blanc' : ('a2','b1','a4','b3','c2','d1','a6','b5','c4','d3','e2','f1','a8','b7','c6','d5','e4','f3','g2','h1','c8','d7','e6','f5','g4','h3','e8','f7','g6','h5','g8','h7'),
-            'fou noir' : ('a1','a3','b2','c1','a5','b4','c3','d2','e1','a7','b6','c5','d4','e3','f2','g1','b8','c7','d6','e5','f4','g3','h2','d8','e7','f6','g5','h4','f8','g7','h6','h8'),
-            'cavalier' : (),
-            'tour' : (),
-            'reine' : (),
-            'roi' : ()}
